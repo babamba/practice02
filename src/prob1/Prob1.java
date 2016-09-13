23cf503947683c0fbe5e60748e40fc91f6f9b6e4
bit-user <orochi13@naver.com>
2016-09-13 16:41:29 +0900

    1

--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -8,8 +8,19 @@ public class Prob1 {
 		int sum = 0;
 
 		/* 여기에 코드를 작성합니다. */
-		
-		System.out.println( "주어진 배열에서 3의 배수의 개수=>" + count );
+		// 배열 3의 배수 구하는 코드
+		// 배열 3의 배수의 합 구하는 코드
+
+		for(int i = 0; i < data.length; i++){
+			if(data[i]%3 == 0){ //3의 배수
+				count++;
+				sum += data[i];  
+//				sum = sum + data[i];
+			}
+//			System.out.println(" + count );								
+//			System.out.println(data[i]);
+		}		
+		System.out.println( "주어진 배열에서 3의 배수의 개수=> " + count);				
 		System.out.println( "주어진 배열에서 3의 배수의 합=>" + sum );
 	}
 }
